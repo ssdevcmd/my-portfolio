fd0df3144f87705524dba96a5e6a73e8fdad25da
ssdevcmd <ssph079@gmail.com>
2026-09-13 22:05:11 +0600

    update one things

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
 
 export default function Home() {
   return (
